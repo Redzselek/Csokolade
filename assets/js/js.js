d2c991851283c3b9643659d6fef5f120511b8f59
Egyedi Róbert <egyedirobika@gmail.com>
2023-09-27 13:56:17 +0200

    2.óra vége

--- assets/js/js.js
+++ assets/js/js.js
@@ -24,9 +24,9 @@ var csokik = [
         orszag: "250g"
     },
     {
-        nev: "",
-        kep: "Boci.png",
-        kiszereles: "100g",
+        nev: "Bere Nándi",
+        kep: "berenandi.png",
+        kiszereles: "70kg",
         orszag: "Magyarország"
     }
 ]
@@ -35,11 +35,27 @@ var kep = 0;
 
 Kirajzol = (t, x) => {
     var s = "";
-    s = "<img class='w-100' src='assets/images/"+t[x].kep+"'>"
-    s += "<h1>"+t[x].nev+"</h1>"
-    s += "<p>"+t[x].kiszereles+"</p>"
+    s = "<img class=' animate__animated animate__slideInLeft  w-100' src='assets/images/"+t[x].kep+"'>"
+    s += "<h1 class='animate__animated animate__fadeInLeft'>"+t[x].nev+"</h1>"
+    s += "<p class='animate__animated animate__fadeInRight'>"+t[x].kiszereles+"</p>"
     s += "<p class='orszag'>"+t[x].orszag+"</p>"
     document.getElementById("kep").innerHTML = s
+/*     document.getElementById("kep").style.display = "none"
+    document.getElementById("kep").style.display = "block" */
+
+    
 }
 
 Kirajzol(csokik,kep)
+
+const Lapoz =(irany) => {
+    kep += irany
+    if (kep<0) {
+        kep = csokik.length-1
+    }
+    else if (kep == csokik.length)
+    {
+        kep = 0
+    }
+    Kirajzol(csokik, kep)
+}
